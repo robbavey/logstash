993081a06106a1e5a55551cef70c162936883632
Rob Bavey <rob.bavey@elastic.co>
2020-06-17 18:05:22 -0400

    Wait for elasticsearch to stop before removing files

--- qa/integration/services/elasticsearch_teardown.sh
+++ qa/integration/services/elasticsearch_teardown.sh
@@ -7,9 +7,15 @@ source "$current_dir/helpers.sh"
 ES_HOME="$current_dir/../../../build/elasticsearch"
 
 stop_es() {
+    local count=10
     pid=$(cat $ES_HOME/elasticsearch.pid)
     [ "x$pid" != "x" ] && [ "$pid" -gt 0 ]
-    kill -SIGTERM $pid
+    while kill -SIGTERM "$pid" 2>/dev/null && [[ $count -ne 0 ]]; do
+       echo "waiting for elasticsearch to stop"
+       count=$(( $count - 1 ))
+       [[ $count -eq 0 ]] && kill -9 $pid
+       sleep 0.5
+    done
 }
 
 stop_es
